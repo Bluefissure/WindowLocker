637ef32bbec2ca8de220367e4ba06ee17f40531f
Bluefissure <bluefissure@foxmail.com>
2021-06-11 18:03:09 -0400

    Add project files.

---
+++ WindowLocker/Plugin.cs
@@ -0,0 +1,85 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.Plugin;
+using Dalamud.Hooking;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace WindowLocker
+{
+    public class Plugin : IDalamudPlugin
+    {
+        public string Name => "Window Locker";
+
+        private const string commandName = "/windowlocker";
+        internal string lastMovedWindowName = "";
+
+        private DalamudPluginInterface pi;
+        internal Configuration config;
+        private PluginUI ui;
+
+        private IntPtr setUiPositionAddress = IntPtr.Zero;
+        private delegate IntPtr SetUiPositionDelegate(IntPtr _this, IntPtr uiObject, ulong y);
+        private Hook<SetUiPositionDelegate> setUiPositionHook;
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            this.pi = pluginInterface;
+
+
+            setUiPositionAddress = this.pi.TargetModuleScanner.ScanText("40 53 48 83 EC 20 80 A2 ?? ?? ?? ?? ??");
+            setUiPositionHook = new Hook<SetUiPositionDelegate>(setUiPositionAddress, new SetUiPositionDelegate(SetUiPositionDetour));
+            setUiPositionHook.Enable();
+
+            this.config = this.pi.GetPluginConfig() as Configuration ?? new Configuration();
+            this.config.Initialize(this.pi);
+
+            this.ui = new PluginUI(this);
+
+            this.pi.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "A useful message to display in /xlhelp"
+            });
+
+
+            this.pi.UiBuilder.OnBuildUi += DrawUI;
+            this.pi.UiBuilder.OnOpenConfigUi += (sender, args) => DrawConfigUI();
+        }
+
+        private unsafe IntPtr SetUiPositionDetour(IntPtr _this, IntPtr uiObject, ulong a3)
+        {
+            string windowName = Marshal.PtrToStringAnsi(uiObject + 8);
+            PluginLog.Information($"windowName: {windowName}");
+            lastMovedWindowName = windowName;
+            if (this.config.lockedWindows.Contains(windowName))
+                return IntPtr.Zero;
+            return setUiPositionHook.Original(_this, uiObject, a3);
+        }
+
+        public void Dispose()
+        {
+            setUiPositionHook.Disable();
+            this.ui.Dispose();
+
+            this.pi.CommandManager.RemoveHandler(commandName);
+            this.pi.Dispose();
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // in response to the slash command, just display our main ui
+            this.ui.Visible = true;
+        }
+
+        private void DrawUI()
+        {
+            this.ui.Draw();
+        }
+
+        private void DrawConfigUI()
+        {
+            this.ui.Visible = true;
+        }
+    }
+}
---
+++ WindowLocker/PluginUI.cs
@@ -0,0 +1,74 @@
+﻿using ImGuiNET;
+using System;
+using System.Numerics;
+
+namespace WindowLocker
+{
+    // It is good to have this be disposable in general, in case you ever need it
+    // to do any cleanup
+    class PluginUI : IDisposable
+    {
+        private Plugin plugin;
+
+        // this extra bool exists for ImGui, since you can't ref a property
+        private bool visible = false;
+        public bool Visible
+        {
+            get { return this.visible; }
+            set { this.visible = value; }
+        }
+
+
+        // passing in the image here just for simplicity
+        public PluginUI(Plugin _plugin)
+        {
+            this.plugin = _plugin;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Draw()
+        {
+            DrawWindow();
+        }
+
+
+        public void DrawWindow()
+        {
+            if (!visible)
+            {
+                return;
+            }
+
+            ImGui.SetNextWindowSize(new Vector2(232, 500), ImGuiCond.FirstUseEver);
+            if (ImGui.Begin("Window Locker", ref this.visible))
+            {
+                ImGui.Text("Last Moved Window:");
+                ImGui.Text(this.plugin.lastMovedWindowName);
+                ImGui.SameLine(ImGui.GetColumnWidth() - 20);
+                if (ImGui.Button("Lock")) {
+                    string windowName = this.plugin.lastMovedWindowName;
+                    if (windowName!= "" && !this.plugin.config.lockedWindows.Contains(windowName))
+                    {
+                        this.plugin.config.lockedWindows.Add(windowName);
+                        this.plugin.config.Save();
+                    }
+                }
+                ImGui.Separator();
+                ImGui.Text("Locked Windows:");
+                foreach(var name in this.plugin.config.lockedWindows)
+                {
+                    if (ImGui.Selectable(name))
+                    {
+                        this.plugin.config.lockedWindows.Remove(name);
+                        this.plugin.config.Save();
+                        break;
+                    }
+                }
+            }
+            ImGui.End();
+        }
+    }
+}
